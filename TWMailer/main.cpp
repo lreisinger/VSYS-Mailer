a25850517f2ce46b31183954d3df9bd452bdfb22
Dominik Hofmann <hofmann_dominik@gmx.at>
2015-10-18 17:10:59 +0200

    cstring include

--- TWMailer/main.cpp
+++ TWMailer/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <cstring>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
@@ -21,46 +22,46 @@ int main(int argc, const char * argv[]) {
     int conn_fd;
     struct sockaddr_in my_addr;
     struct sockaddr_in client_addr;
-    
+
     int maxlen = 1024;
     char buffer[maxlen];
-    
+
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&my_addr, 0, sizeof(my_addr));
-    
+
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(6002);
     my_addr.sin_addr.s_addr = htons(INADDR_ANY);
-    
+
     if( ::bind(socket_fd, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1){
         perror("bind error");
         return EXIT_FAILURE;
     }
-    
+
     if(listen(socket_fd, queue) == -1)
     {
         perror("listen error");
         exit(1);
     }
-    
+
     socklen_t client_addrlen = sizeof(client_addr);
-    
+
     while (true) {
         cout << "Waiting for Connection..." << endl;
         conn_fd = accept (socket_fd, (sockaddr *) &client_addr, &client_addrlen);
-        
+
         if (conn_fd > 0)
         {
             cout << "Client: " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << endl;
-            
+
             //strcpy(buffer,"Hallo");
             //send(conn_fd, buffer, strlen(buffer),0);
         }
-        
-        
+
+
         while(true)
         {
-            
+
             ssize_t recv_len = recv(conn_fd, &buffer, maxlen-1, 0);
             buffer[maxlen-1] = '\0';
             if(recv_len == -1){
@@ -76,10 +77,10 @@ int main(int argc, const char * argv[]) {
             {
                 cout << buffer << endl;
             }
-            
+
         }
-        
+
     }
-    
+
     return 0;
 }
