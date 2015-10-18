1fde04101c010fc9ac9d7562e5b185ab37e26aa9
Dominik Hofmann <hofmann_dominik@gmx.at>
2015-10-18 21:02:41 +0200

    TEST

--- TWMailer/main.cpp
+++ TWMailer/main.cpp
@@ -2,8 +2,8 @@
 //  main.cpp
 //  TWMailer
 //
-//  Created by Lukas Reisinger on 08.10.15.
-//  Copyright (c) 2015 Lukas Reisinger. All rights reserved.
+//  Created by Lukas Reisinger and Dominik Hofmann.
+//  Copyright (c) 2015 Lukas Reisinger and Dominik Hofmann. All rights reserved.
 //
 
 #include <iostream>
