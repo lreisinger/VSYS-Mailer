d6bf93af1be9146041a283ee0eeaaa9987c7430a
Dominik Hofmann <hofmann_dominik@gmx.at>
2015-10-18 23:49:50 +0200

    Send funktionert am Client

--- TWMailer/client.cpp
+++ TWMailer/client.cpp
@@ -9,6 +9,9 @@
 #define BUF 1024
 #define PORT 6002
 
+int handleInput(char* input);
+int sendMail(int create_socket);
+
 int main (int argc, char **argv) {
   int create_socket;
   char buffer[BUF];
@@ -50,9 +53,71 @@ int main (int argc, char **argv) {
   do {
      printf ("Send message: ");
      fgets (buffer, BUF, stdin);
-     send(create_socket, buffer, strlen (buffer), 0);
+     printf("%d\n",handleInput(buffer));
+     switch (handleInput(buffer)) {
+        case -1: printf("Quitting Client...\n");break;
+        case 1: sendMail(create_socket); break; //SEND
+        case 2: break; //LIST
+        case 3: break; //READ
+        case 4: break; //DEL
+        default: printf("Unknown Command");
+     }
   }
-  while (strcmp (buffer, "quit\n") != 0);
+  while (strcasecmp(buffer, "quit\n") != 0);
   close (create_socket);
   return EXIT_SUCCESS;
+}
+
+int handleInput(char* input) {
+    if (strcasecmp(input,"QUIT\n")==0)
+        return -1;
+    else if (strcasecmp(input,"SEND\n")==0)
+        return 1;
+    else if (strcasecmp(input,"LIST\n")==0)
+        return 2;
+    else if (strcasecmp(input,"READ\n")==0)
+        return 3;
+    else if (strcasecmp(input,"DEL\n")==0)
+        return 4;
+    else
+        return 0;
+}
+
+int sendMail(int create_socket) {
+    char from[9], to[9], subject[81], message[901], msgBuffer[901];
+
+    printf("From: ");
+    fgets(from, 9, stdin);
+    getc(stdin);
+
+    printf("To: ");
+    fgets(to, 9, stdin);
+    getc(stdin);
+
+    printf("Subject: ");
+    fgets(subject, 81, stdin);
+
+    printf("Message: ");
+    while (1) {
+        fgets(msgBuffer, 901, stdin);
+        if (strcmp(msgBuffer,".\n")==0)
+            break;
+        strcat(message,msgBuffer);
+    }
+
+    //Protokoll bauen
+
+    char buffer[BUF];
+    strcat(buffer, "SEND\n");
+    strcat(buffer, from);
+    strcat(buffer, "\n");
+    strcat(buffer, to);
+    strcat(buffer, "\n");
+    strcat(buffer, subject);
+    //strcat(buffer, "\n");
+    strcat(buffer, message);
+    strcat(buffer, ".\n");
+
+    send(create_socket, buffer, strlen (buffer), 0);
+    return 0;
 }
