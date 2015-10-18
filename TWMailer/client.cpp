56c08495a05a2ef11475cae7659fbb36ab894713
Dominik Hofmann <hofmann_dominik@gmx.at>
2015-10-19 00:24:46 +0200

    Send wartet auf OK/ERR

--- TWMailer/client.cpp
+++ TWMailer/client.cpp
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 #define BUF 1024
-#define PORT 6002
+#define PORT 6001
 
 int handleInput(char* input);
 int sendMail(int create_socket);
@@ -56,7 +56,12 @@ int main (int argc, char **argv) {
      printf("%d\n",handleInput(buffer));
      switch (handleInput(buffer)) {
         case -1: printf("Quitting Client...\n");break;
-        case 1: sendMail(create_socket); break; //SEND
+        case 1:
+            if (sendMail(create_socket))
+                printf("Your message was sent!\n");
+            else
+                printf("Your message could not be sent!\n");
+            break;
         case 2: break; //LIST
         case 3: break; //READ
         case 4: break; //DEL
@@ -119,5 +124,11 @@ int sendMail(int create_socket) {
     strcat(buffer, ".\n");
 
     send(create_socket, buffer, strlen (buffer), 0);
-    return 0;
+
+    recv(create_socket,buffer,BUF-1, 0);
+
+    if (strcasecmp(buffer,"OK\n")==0)
+        return 1;
+    else
+        return 0;
 }
